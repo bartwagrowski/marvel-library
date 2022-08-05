7c5c6fe199653371e95787ef3512d7a59dabccdc
Bart Wagrowski <bartlomiej.wagrowski@gmail.com>
2022-08-05 10:26:21 +0100

    Added the test for the details state hook

---
+++ src/components/character-details/__tests__/use-character-details-state.test.js
@@ -0,0 +1,39 @@
+import { renderHook, waitFor } from '@testing-library/react';
+import useCharacterDetailsState from '../use-character-details-state';
+import { getCharacterDetails } from '../../../api/marvel-api';
+
+jest.mock('react-router-dom', () => ({
+    useParams: () => ({
+        characterId: 'A12345',
+    }),
+}));
+
+jest.mock('../../../api/marvel-api', () => ({
+    getCharacterDetails: jest.fn(),
+}));
+
+describe('useCharacterDetailsState hook', () => {
+    it('should initialise correctly if the data call succeeds', async () => {
+        getCharacterDetails.mockResolvedValue({ data: { results: [{ foo: 'bar' }] } });
+        const { result } = renderHook(() => useCharacterDetailsState());
+
+        await waitFor(() => {
+            expect(result.current.character).toEqual({ foo: 'bar' });
+        });
+
+        expect(result.current.error).toBeNull();
+        expect(result.current.isLoading).toBeFalsy();
+    });
+
+    it('should initialise correctly if the data call errors', async () => {
+        getCharacterDetails.mockRejectedValue(new Error('Oh no!'));
+        const { result } = renderHook(() => useCharacterDetailsState());
+
+        await waitFor(() => {
+            expect(result.current.error).toEqual('Oh no!');
+        });
+
+        expect(result.current.character).toBeNull();
+        expect(result.current.isLoading).toBeFalsy();
+    });
+});
